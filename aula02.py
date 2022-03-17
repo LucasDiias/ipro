f7b25aaa9bc14aa55e9363aec68af1fc72269a73
Lucas Dias <maykonlucasdias@gmail.com>
2022-03-17 15:09:45 -0300

    Update aula02.py

--- aula02.py
+++ aula02.py
@@ -1,8 +1,20 @@
-numeros = 0
+numero = 1
+soma = 0
 
 print('Insira 5 números a serem somados.\n')
 
-for i in range(5):
-    numeros += int(input(f'Insira o {i + 1}º inteiro: '))
+while numero != 0:
+    numero = int(input(f'Insira um inteiro: '))
+    soma += numero
 
-print(f'\nA soma dos números inseridos é: {numeros}')
+# Com While
+# i = 0
+# while i < 5:
+#     numeros += int(input(f'Insira o {i + 1}º inteiro: '))
+#     i += 1
+
+# Com For
+# for i in range(5):
+#     numeros += int(input(f'Insira o {i + 1}º inteiro: '))
+
+print(f'\nA soma dos números inseridos é: {soma}')
