530700941f6b55c7d0cc13179542a9a8d574462b
Lucas Dias <maykonlucasdias@gmail.com>
2022-03-17 16:13:43 -0300

    Update aula02.py

--- aula02.py
+++ aula02.py
@@ -6,14 +6,4 @@ while soma <= 50:
     numero = int(input(f'Insira um inteiro: '))
     soma += numero
 
-# Com While
-# i = 0
-# while i < 5:
-#     numeros += int(input(f'Insira o {i + 1}º inteiro: '))
-#     i += 1
-
-# Com For
-# for i in range(5):
-#     numeros += int(input(f'Insira o {i + 1}º inteiro: '))
-
 print(f'\nA soma dos números inseridos é: {soma}')
