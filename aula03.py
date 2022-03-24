4263143d1ff42162985f3cac294355e497f5ef10
Lucas Dias <maykonlucasdias@gmail.com>
2022-03-24 15:00:01 -0300

    Update aula03.py

--- aula03.py
+++ aula03.py
@@ -1,13 +1,13 @@
 from random import randint
 inicio = 1
 fim = 10
+tentativas = 0
 
 numero = randint(1, 10)
 escolha = int(
     input('Adivinhe um número entre 1 e 10: '))
 print(numero)
 while escolha != numero and inicio != fim:
-
     if escolha < numero and escolha >= inicio:
         inicio = escolha + 1
     elif escolha > numero and escolha <= fim:
@@ -18,6 +18,9 @@ while escolha != numero and inicio != fim:
     else:
         print('Você errou! Tente novamente.')
         escolha = int(input(f'Adivinhe um número entre {inicio} e {fim}: '))
+    tentativas += 1
 
 if escolha == numero:
     print(f'\nVocê acertou! O número escolhido pelo computador era: {numero}')
+
+print(f'Você tentou {tentativas} vezes.')
