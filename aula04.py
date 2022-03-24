5fbeedfad8696b776f61df3be1315cacaa02485f
Lucas Dias <maykonlucasdias@gmail.com>
2022-03-24 16:29:48 -0300

    Add aula04.py

---
+++ aula04.py
@@ -0,0 +1,16 @@
+usuarios = 0
+soma = 0
+
+i = 1
+while True:
+    idade = int(input(f'Usuário {i}, informe sua idade ou zero para sair: '))
+
+    if idade == 0:
+        break
+
+    soma += idade
+    usuarios += 1
+    i += 1
+
+
+print(f'A média das idades apresentadas é de: {soma / usuarios:.2f}')
