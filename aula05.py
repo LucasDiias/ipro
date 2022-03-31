a68166921f6a1b46457435ac85cc9a8e6765fb5a
Lucas Dias <maykonlucasdias@gmail.com>
2022-03-31 16:38:27 -0300

    Add aula05.py

---
+++ aula05.py
@@ -0,0 +1,24 @@
+from os import system as sys
+
+alunos = 'Niaclara', 'Ramon', 'Detetive Will', 'Pedro Danilo', 'Pedro', 'Lucas', 'Rodrigo', 'Sofia', 'Helena', 'Paulo', 'Bia', 'Renata', 'Otávio', 'Maju', 'Carlos Eduardo', 'Vitor Hugo'
+
+presentes = 0
+
+for aluno in sorted(alunos):
+    presenca = input(f'{aluno} está presente? (P/A) ').upper()
+
+    while presenca != 'P' or presenca != 'A':
+        presenca = input(
+            f'Valor inválido. {aluno} está presente? (P/A) ').upper()
+
+    if presenca == 'P':
+        presentes += 1
+
+sys('cls')
+
+print('---'*20)
+print('Relatório de Frequência - 921A.2\n')
+print(f'Total: {len(alunos)}')
+print(f'Presentes: {presentes}')
+print(f'Ausentes: {len(alunos) - presentes}')
+print('---'*20)
