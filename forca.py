ff3c79d3574e55610e206ac15e7f96cfb2ed73a9
Lucas Dias <63432976+LucasDiias@users.noreply.github.com>
2022-04-30 00:35:56 -0300

    Update forca.py

--- forca.py
+++ forca.py
@@ -16,7 +16,7 @@ try:
     while qtdeChutes > 0 and tracos != palavra:
         chute = input('\nAdivinhe uma letra: ').strip().lower()
 
-        if len(chute) > 1 and chute:
+        if len(chute) = 1:
             print('Você só pode tentar letras.')
         else:
             if chute not in chutes:
